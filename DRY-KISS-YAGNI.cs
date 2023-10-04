c5784eeb4c9fae9e6cb382c6720d0734084095b9
Mainak Halder <mainakhalder5598633@gmail.com>
2023-10-04 20:16:04 +0530

    DRY-KISS-YAGNI

---
+++ DRY-KISS-YAGNI.cs
@@ -0,0 +1,14 @@
+//DRY -> DONT REPEAT YOURSELF
+// Every piece of knowledge must have a single, unambiguous, authoritative representation within a system.
+//This means that you should not have duplicated code. It’s easier to maintain a code that is only in one place, 
+//because if you need to change something in the code, you just need to change in one place.
+
+// KISS -> KEEP IT SIMPLE, STUPID
+//This means that you should not have duplicated code. It’s easier to maintain a code that is only in
+// one place, because if you need to change something in the code, you just need to change in one place.
+
+//YAGNI -> You Ain’t Gonna Need It.
+//Always implement things when you actually need them, never when you just foresee that you need them.
+//It means that you should not implement functionality just because you think that you may need it someday,
+// but implement it just when you really need it. Doing that you will avoid spending time with implementations
+// that were not even necessary, and maybe will never be used.
